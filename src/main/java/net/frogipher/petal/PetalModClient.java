cf242346421167d3922658cd25aade8844e136dc
Julius Rodriguez <juliusrodriguez@Juliuss-MacBook-Pro.local>
2023-11-06 00:24:53 -0500

    Initial commit

---
+++ src/main/java/net/frogipher/petal/PetalModClient.java
@@ -0,0 +1,10 @@
+package net.frogipher.petal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PetalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
